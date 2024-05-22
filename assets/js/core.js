067a040795a2b9a5a2eae3b35854a3cf5f3f9b03
kanishk-reply <160497361+kanishk-reply@users.noreply.github.com>
2024-05-22 13:29:11 +0100

    Update core.js

--- assets/js/core.js
+++ assets/js/core.js
@@ -21,6 +21,37 @@ var Exchange = function() {
 
         userWon : userWon,
 
+        const socket = new WebSocket('ws://localhost:8765');
+
+        socket.onopen = function() {
+            console.log('WebSocket connection established');
+        };
+        
+        socket.onmessage = function(event) {
+            console.log('Message from server: ', event.data);
+            if (event.data === "hard_refresh") {
+                console.log('Performing hard refresh');
+                location.reload(true); // Forces a hard refresh
+                console.log('Hard refresh complete');
+            }
+        };
+        
+        socket.onclose = function() {
+            console.log('WebSocket connection closed');
+        };
+        
+        function sendStart() {
+            const message = 'Game Started';
+            socket.send(message);
+            console.log('Message sent: ', message);
+        }
+        
+        function sendReset() {
+            const message = 'New Game';
+            socket.send(message);
+            console.log('Message sent: ', message);
+        }
+
         initHelper: function(helper) {
             switch (helper) {
                 case 'easy-pie-chart':

66f29955fa3a3e0015470e4d6d58ff0a08a59dd3
kanishk-reply <160497361+kanishk-reply@users.noreply.github.com>
2024-05-23 13:29:56 +0100

    Update core.js

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,6 +1,6 @@
 var Exchange = function() {
 
-    let bugLeft = '10';                
+    let bugLeft = '1';                
     let gameOver = false;
     let userWon = false;
     let pauseTimer = false;
